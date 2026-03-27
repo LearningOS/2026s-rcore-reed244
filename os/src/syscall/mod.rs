a455af375137a000c1d809806e074b8af745c926
reed244 <2158493577@qq.com>
2026-03-27 17:50:28 +0800

    test

--- os/src/syscall/mod.rs
+++ os/src/syscall/mod.rs
@@ -21,6 +21,15 @@ const SYSCALL_GET_TIME: usize = 169;
 /// trace syscall
 const SYSCALL_TRACE: usize = 410;
 
+// trace request for sys_trace
+mod called_times {
+    pub static mut WRITE: usize = 0;
+    pub static mut EXIT: usize = 0;
+    pub static mut YIELD: usize = 0;
+    pub static mut GET_TIME: usize = 0;
+    pub static mut TRACE: usize = 0;
+}
+
 mod fs;
 mod process;
 
@@ -30,11 +39,36 @@ use process::*;
 /// handle syscall exception with `syscall_id` and other arguments
 pub fn syscall(syscall_id: usize, args: [usize; 3]) -> isize {
     match syscall_id {
-        SYSCALL_WRITE => sys_write(args[0], args[1] as *const u8, args[2]),
-        SYSCALL_EXIT => sys_exit(args[0] as i32),
-        SYSCALL_YIELD => sys_yield(),
-        SYSCALL_GET_TIME => sys_get_time(args[0] as *mut TimeVal, args[1]),
-        SYSCALL_TRACE => sys_trace(args[0], args[1], args[2]),
+        SYSCALL_WRITE => {
+            unsafe {
+                called_times::WRITE += 1;
+            }
+            sys_write(args[0], args[1] as *const u8, args[2])
+        }
+        SYSCALL_EXIT => {
+            unsafe {
+                called_times::EXIT += 1;
+            }
+            sys_exit(args[0] as i32)
+        }
+        SYSCALL_YIELD => {
+            unsafe {
+                called_times::YIELD += 1;
+            }
+            sys_yield()
+        }
+        SYSCALL_GET_TIME => {
+            unsafe {
+                called_times::GET_TIME += 1;
+            }
+            sys_get_time(args[0] as *mut TimeVal, args[1])
+        }
+        SYSCALL_TRACE => {
+            unsafe {
+                called_times::TRACE += 1;
+            }
+            sys_trace(args[0], args[1], args[2])
+        }
         _ => panic!("Unsupported syscall_id: {}", syscall_id),
     }
 }
--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -40,6 +40,27 @@ pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
 
 // TODO: implement the syscall
 pub fn sys_trace(_trace_request: usize, _id: usize, _data: usize) -> isize {
-    trace!("kernel: sys_trace");
-    -1
+    match _trace_request {
+        0 => {
+            let addr = id as *const u8;
+            return unsafe { *addr as isize };
+        }
+        1 => {
+            let mut addr = id as *mut u8;
+            unsafe {
+                // only write last byte
+                *addr = data as u8;
+                return 0;
+            }
+        }
+        2 => match id {
+            64 => unsafe { called_times::WRITE },
+            93 => unsafe { called_times::EXIT },
+            124 => unsafe { called_times::YIELD },
+            169 => unsafe { called_times::GET_TIME },
+            410 => unsafe { called_times::TRACE },
+            _ => return -1,
+        },
+    }
+    return -1;
 }
